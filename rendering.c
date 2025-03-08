d6a7506e70742bb40afeb98b4dab47201d87d092
1-veer <ayoub1bougatia@gmail.com>
2025-03-08 18:06:58 +0000

    still

--- rendering.c
+++ rendering.c
@@ -6,13 +6,28 @@
 /*   By: abougati <abougati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 15:00:55 by abougati          #+#    #+#             */
-/*   Updated: 2025/03/08 16:28:46 by abougati         ###   ########.fr       */
+/*   Updated: 2025/03/08 17:09:54 by abougati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Includes/fract_ol.h"
 
-// void rendering (t_fract *fract)
-// {
+void   pixel_handler(int x, int y, t_fract *fract)
+{
     
-// }
+}
+
+ void rendering (t_fract *fract)
+{
+    int y;
+    int x;
+    
+    y = 0;
+    while (y++ < 1000)
+        {
+            x = 0;
+            while (x++ < 1000)
+                pixel_handler(x, y, fract);
+        }    
+    
+}
