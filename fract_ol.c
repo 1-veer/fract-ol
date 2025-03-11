f57aaa0bb521cbb7f7abdabe7c6e91f13ce1ced9
1-veer <ayoub1bougatia@gmail.com>
2025-03-11 21:25:05 +0000

    donnneee

--- fract_ol.c
+++ fract_ol.c
@@ -6,11 +6,23 @@
 /*   By: abougati <abougati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 02:05:23 by abougati          #+#    #+#             */
-/*   Updated: 2025/03/11 02:00:39 by abougati         ###   ########.fr       */
+/*   Updated: 2025/03/11 21:12:34 by abougati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract_ol.h"
+
+double	check_overflow(char *s1)
+{
+	double	res;
+
+	res = adapt_inpt(s1);
+	if (res > 2.0)
+		res = 2.0;
+	else if (res < -2.0)
+		res = -2.0;
+	return (res);
+}
 
 int	main(int ac, char *av[])
 {
@@ -22,8 +34,8 @@ int	main(int ac, char *av[])
 		fract.name = av[1];
 		if (av[2] && av[3])
 		{
-			fract.julia_re = adapt_inpt(av[2]);
-			fract.julia_im = adapt_inpt(av[3]);
+			fract.julia_re = check_overflow(av[2]);
+			fract.julia_im = check_overflow(av[3]);
 		}
 		initialize_fract(&fract);
 		rendering(&fract);
